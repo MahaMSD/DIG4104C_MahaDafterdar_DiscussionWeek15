72fb2b11acffd270124e2eceece99e64bd4c2017
Khaled Dafterdar <Khaled Dafterdar>
2017-04-22 12:32:36 -0400

    back to top button

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,3 +14,20 @@ $('a[href*=#]:not([href=#])').click(function() {
         }
     }
 });
+
+/* --- back to top button --- */
+var $backToTop = $('.back-to-top');
+$backToTop.hide();
+
+
+$(window).on('scroll', function() {
+  if ($(this).scrollTop() > 100) {
+    $backToTop.fadeIn();
+  } else {
+    $backToTop.fadeOut();
+  }
+});
+
+$backToTop.on('click', function(e) {
+  $('html, body').animate({scrollTop: 0}, 500);
+});
